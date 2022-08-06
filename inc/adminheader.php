586219c7fcb6fbf422114915d09367a398a46fd0
Muditha Tharuka Batuwangala <81775289+mudithatharuka@users.noreply.github.com>
2022-08-06 22:39:52 +0530

    includes added

---
+++ inc/adminheader.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="inc/css/adminheader.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" integrity="sha256-46r060N2LrChLLb5zowXQ72/iKKNiw/lAmygmHExk/o=" crossorigin="anonymous" />
+</head>
+<body>
+
+	<header>
+
+		<div class="Sitename"><i class="fas fa-bars"></i> BLOCK BUSTER</div><!--Sitename-->
+
+		<div class="Loggedin">Welcome Admin <?php echo $_SESSION['name'];?>! <a href="adminlogout.php"><button>Log Out</button></a></div>
+	</header>
+
+	<div class="Choosehead">
+			<ul>
+				<li><a href="adminhome.php"><button class="Movies" >Movies</button></a></li>
+				<li><a href="tvseries.php"><button class="Tv" >Tv Series</button></a></li>
+				<li><a href="mov-reviews.php"><button class="Movie" >Movie Reviews</button></a></li>
+				<li><a href="tsr-reviews.php"><button class="Series" >Series Reviews</button></a></li>
+				<li><a href="latestnew.php"><button class="Latest" >Latest News</button></a></li>
+				<li><a href="celebrity.php"><button class="Celebrities" >Celebrities</button></a></li>
+				<li><a href="users.php"><button class="Users" >Users</button></a></li>
+				<?php if($_SESSION['admin_id'] == 'ADM_001'){ ?>
+				<li><a href="admins.php?admin_id=<?php echo $_SESSION['admin_id'] ?>"><button class="Admins" >Admins</button></a></li>
+				<?php } ?>
+			</ul>
+		</div><!-- Choosehead -->
+
+	<script>
+			$(document).ready(function(){
+				$('.fa-bars').click(function(){
+					$('.Choosehead ul').slideToggle();
+				});
+			});
+		</script>
+</body>
+</html>
---
+++ inc/signup.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="inc/css/signuplogin.css">
+</head>
+<body>
+	<div class="bg-modal">
+		
+
+		
+			<div class="modal-content">
+			
+			<div class="close">
+				+
+			</div><!--close-->
+
+			<h1>SIGN UP</h1>
+				<form action="index.php" method="post" enctype="multipart/form-data">
+					<label>Name:</label>
+					<input type="text" name="name" placeholder="  Name">
+					<label>YOUR EMAIL:</label>
+					<input type="text" name="email" placeholder="  Email">
+					<label>PASSWORD:</label>
+					<input type="text" name="password" placeholder="  Password">
+					
+					<label>PROFILE PHOTO:</label>
+					<input type="file" name="profile_photo" accept="image/*">
+
+					<div class="sign"><button name="sign" id="sign">SIGN UP</button></div>
+				</form>
+		
+			</div><!--modal-content-->
+		
+
+		</div><!--bg-modal-->
+
+		<script type="text/javascript">
+			document.getElementById('signup').addEventListener('click',function(){
+				document.querySelector('.bg-modal').style.display = 'flex';
+			});
+			
+			document.getElementById('signupresponsive').addEventListener('click',function(){
+				document.querySelector('.bg-modal').style.display = 'flex';
+			});
+
+			document.querySelector('.close').addEventListener('click',function(){
+				document.querySelector('.bg-modal').style.display = 'none';
+			});
+
+		</script>
+</body>
+</html>
